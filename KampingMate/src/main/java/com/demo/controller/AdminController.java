511841f824aeaa15f21b1696724194e25f5df8d1
orin602 <tigers2589@naver.com>
2024-06-07 15:07:09 +0900

    20240607

---
+++ KampingMate/src/main/java/com/demo/controller/AdminController.java
@@ -0,0 +1,248 @@
+package com.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.demo.domain.AdminQnaBoard;
+import com.demo.domain.MemberData;
+import com.demo.domain.askBoard;
+import com.demo.service.AdminService;
+
+@Controller
+@SessionAttributes("loginUser")
+public class AdminController {
+
+    @Autowired
+    AdminService adminService;
+
+    @GetMapping("/admin")
+    public String mainView() {
+        return "admin/admin_login";
+    }
+
+    @PostMapping("/admin/login")
+    public String login(@RequestParam("id") String id,
+                        @RequestParam("password") String password,
+                        Model model) {
+        // 실제 로그인 검증 로직을 사용하여 데이터베이스에서 사용자 정보를 확인합니다.
+        MemberData loginUser = adminService.validateLogin(id, password);
+        if (loginUser != null && loginUser.getUsercode() == 0) {
+            model.addAttribute("error", "You are not authorized to login.");
+            return "admin/admin_login";
+        } else if (loginUser != null) {
+            model.addAttribute("loginUser", loginUser);
+            return "redirect:/admin_main";
+        } else {
+            model.addAttribute("error", "Invalid id or password");
+            return "admin/admin_login";
+        }
+    }
+
+ // 로그아웃 처리
+    @GetMapping("/admin_logout")
+    public String logout(SessionStatus status) {
+        status.setComplete(); // 세션 완료 상태로 설정
+        return "admin/admin_login"; // 로그아웃 후 로그인 화면으로 리다이렉트
+    }
+
+    @GetMapping("/admin_main")
+    public String mainView2(@ModelAttribute("loginUser") MemberData loginUser, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+
+            List<AdminQnaBoard> qnaBoardList = adminService.getAllQnaBoardListMain();
+            List<askBoard> askBoardList = adminService.getAllAskBoardListMain();
+
+            model.addAttribute("askBoardList", askBoardList);
+            model.addAttribute("qnaBoardList", qnaBoardList);
+
+            return "admin/admin_main";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/memberList.do")
+    public String memberInfo(@ModelAttribute("loginUser") MemberData loginUser, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+
+            List<MemberData> memberList = adminService.getAllMemberList();
+            model.addAttribute("memberList", memberList);
+
+            return "admin/boardPage/admin_userlist";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/admin_QnA.do")
+    public String QnABoard(@ModelAttribute("loginUser") MemberData loginUser, Model model, Pageable pageable) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+
+            Page<AdminQnaBoard> qnaBoardPage = adminService.getAllQnaBoardList(PageRequest.of(pageable.getPageNumber(), 10));
+            model.addAttribute("qnaBoardPage", qnaBoardPage);
+
+            return "admin/boardPage/admin_qnaboard";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/qnaRegister.do")
+    public String qnaRegiView(@ModelAttribute("loginUser") MemberData loginUser) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            return "admin/boardForm/qnaForm";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @PostMapping("/qnaReg.do")
+    public String qnaRegister(@ModelAttribute("loginUser") MemberData loginUser, AdminQnaBoard vo) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            adminService.insertAdminQnaBoard(vo);
+            return "redirect:/admin_QnA.do";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @PostMapping("/qnaEdit.do")
+    public String editQna(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("qnaSelect") int boardnum, RedirectAttributes redirectAttributes) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            AdminQnaBoard qna = adminService.getByQnaBoardnum(boardnum);
+            redirectAttributes.addFlashAttribute("qna", qna);
+            return "redirect:/qnaEditForm.do?boardnum=" + boardnum;
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/qnaEditForm.do")
+    public String showEditQnaForm(@RequestParam("boardnum") int boardnum, Model model) {
+        if (!model.containsAttribute("qna")) {
+            AdminQnaBoard qna = adminService.getByQnaBoardnum(boardnum);
+            model.addAttribute("qna", qna);
+        }
+        return "admin/boardForm/qnaEditForm";
+    }
+
+    @PostMapping("/qnaUpdate.do")
+    public String qnaUpdate(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("qnaNum") int boardnum, AdminQnaBoard vo) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            vo.setQna_boardnum(boardnum);
+            adminService.updateAdminQnaBoard(vo);
+            return "redirect:/admin_QnA.do";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @PostMapping("/qnaDelete.do")
+    public String deleteQna(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("qnaSelect") int boardnum) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            adminService.deleteAdminQnaBoard(boardnum);
+            return "redirect:/admin_QnA.do";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/admin_ask.do")
+    public String AskBoard(@ModelAttribute("loginUser") MemberData loginUser, Pageable pageable, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            Page<askBoard> askBoardPage = adminService.getAllAskBoardListWait(PageRequest.of(pageable.getPageNumber(), 10));
+            model.addAttribute("askBoardPage", askBoardPage);
+            return "admin/boardPage/admin_askboard";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @PostMapping("/admin_ask.do")
+    public String AskBoardFinish(@ModelAttribute("loginUser") MemberData loginUser, RedirectAttributes redirectAttributes, Pageable pageable, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            Page<askBoard> askBoardPage = adminService.getAllAskBoardListFinish(PageRequest.of(pageable.getPageNumber(), 10));
+            model.addAttribute("askBoardPage", askBoardPage);
+            redirectAttributes.addFlashAttribute("askBoardPage", askBoardPage);
+            return "redirect:/admin_ask_board.do";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/admin_ask_board.do")
+    public String showFinishAsk(@ModelAttribute("loginUser") MemberData loginUser) {
+        if (loginUser != null && loginUser.getId() != null) {
+            return "admin/boardPage/admin_askboardEnd";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/askDetail.do")
+    public String askDetail(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("askNum") int askNum, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            askBoard ask = adminService.getByAskBoardnum(askNum);
+            model.addAttribute("ask", ask);
+            model.addAttribute("end_view", "view");
+            return "admin/boardForm/askEditForm";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/askEdit.do")
+    public String askEdit(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("askNum") int askNum, Model model) {
+        if (loginUser != null && loginUser.getId() != null) {
+            adminService.adminCheck(loginUser);
+            askBoard ask = adminService.getByAskBoardnum(askNum);
+            model.addAttribute("ask", ask);
+            return "admin/boardForm/askEditForm";
+        } else {
+            return "redirect:/admin";
+        }
+    }
+
+    @PostMapping("/askUpdate.do")
+    public String askUpdate(@ModelAttribute("loginUser") MemberData loginUser, @RequestParam("askNum") Long boardnum, 
+        askBoard vo) {
+        askBoard ask = adminService.getByAskBoardnum(boardnum);
+        vo.setRegdate(ask.getRegdate());
+        vo.setInquiry_id(boardnum);
+        vo.setEmail(ask.getEmail());
+        vo.setName(ask.getName());
+        vo.setStatus("답변 완료");
+        vo.setMember_data(loginUser);
+        
+        adminService.adminCheck(loginUser);
+        adminService.updateAdminInquiry(vo);
+        
+        // askUpdate.html 템플릿으로 리다이렉트합니다.
+        return "redirect:/admin_ask.do";
+    }
+
+}
---
+++ KampingMate/src/main/java/com/demo/controller/HomeController.java
@@ -0,0 +1,57 @@
+package com.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.client.RestTemplate;
+
+import com.demo.domain.MemberData;
+import com.demo.service.GoCampingAPI;
+import com.demo.service.GoCampingAPI.CampingApiResponse;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+	    
+    @GetMapping("/")
+    public String index() {
+        return "index"; // index.html 파일을 반환합니다.
+    }
+
+    @GetMapping("/main")
+    public String main(Model model, HttpSession session) {
+        // 세션에서 loginUser를 가져옵니다.
+        MemberData loginUser = (MemberData) session.getAttribute("loginUser");
+        
+        // 로그인되지 않은 경우 로그인 페이지로 리다이렉트합니다.
+        if (loginUser == null) {
+            return "main";
+        }
+
+        // 로그인된 사용자의 이름을 모델에 추가합니다.
+        model.addAttribute("name", loginUser.getName());
+
+        return "main"; // main.html 파일을 반환합니다.
+    }
+    
+    @GetMapping("/introduce")
+    public String introduce() {
+    	return "introduce";
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm"; // loginForm.html을 렌더링합니다.
+    }
+    
+    // 로그아웃 처리
+    @GetMapping("/logout")
+    public String logout(SessionStatus status) {
+        status.setComplete(); // 세션 완료 상태로 설정
+        return "main"; // 로그아웃 후 로그인 화면으로 리다이렉트
+    }
+}

a48c097fbb6353f9716338edab22bbd0d52af7f3
jkzk7821 <167295101+jkzk7821@users.noreply.github.com>
2024-06-21 13:09:00 +0900

    Add files via upload

---
+++ 수정사항(파일 업로드)/06.21 수정사항-추천기록/SearchHistoryRepository.java
@@ -0,0 +1,30 @@
+package com.demo.persistence;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.demo.domain.GoCamping;
+import com.demo.domain.SearchHistory;
+
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Integer> {
+	
+	 @Query("SELECT CASE WHEN COUNT(sh) > 0 THEN true ELSE false END FROM SearchHistory sh WHERE sh.gocamping.contentId = :contentId AND sh.member.no_data = :no_data")
+	 boolean existsByGocampingContentIdAndMemberNo_data(@Param("contentId") int contentId, @Param("no_data") Long no_data);
+
+	 @Modifying
+	 @Transactional
+	 @Query(value="INSERT INTO Search_History (history_id, content_Id, no_data, recommend_date) VALUES (HISTORY_SEQ.nextval, :contentId, :no_data, :recommend_date)", nativeQuery = true)
+	 void insertHistoryItem(@Param("contentId") int contentId, @Param("no_data") Long no_data, @Param("recommend_date") Date recommend_date);
+
+	 @Query("SELECT sh.gocamping FROM SearchHistory sh WHERE sh.member.no_data = :no_data")
+	 List<GoCamping> findAllCampingByMemberNoData(@Param("no_data") Long no_data);
+	 
+	 @Query("SELECT sh FROM SearchHistory sh WHERE sh.member.no_data = :no_data")
+	 List<SearchHistory> findAllByMemberNoData(@Param("no_data") Long no_data);
+}

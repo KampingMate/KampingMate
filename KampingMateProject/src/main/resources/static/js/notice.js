8165e21362d90c2e822ce6c6acbf3c8772b5bf58
jaecheon1 <french1022@naver.com>
2024-06-25 14:29:36 +0900

    0625-14:30

--- KampingMateProject/src/main/resources/static/js/notice.js
+++ KampingMateProject/src/main/resources/static/js/notice.js
@@ -127,12 +127,13 @@ function go_page(page) {
 }
 
 function go_list() {
-    document.getElementById('category').value = "";  
-    document.getElementById('page').value = 1;
-
-    var searchForm = document.getElementById('searchForm');
-    searchForm.action = "/notice";  // URL을 /notice로 설정
-    searchForm.submit();
+//    document.getElementById('category').value = "";  
+//    document.getElementById('page').value = 1;
+//
+//    var searchForm = document.getElementById('searchForm');
+//    searchForm.action = "/notice";  // URL을 /notice로 설정
+//    searchForm.submit();
+		history.back();
 }
 
 	
